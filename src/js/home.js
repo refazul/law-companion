1d0976aceaf593b15ec91ba11d5ee9ccb5681730
unknown <talhajubaer018@gmail.com>
2019-07-13 19:47:52 +0600

    before page merge

--- src/js/home.js
+++ src/js/home.js
@@ -13,39 +13,42 @@ import Topbanner from 'topbanner';
 import { navbar_data, causelist_data, sidebar1_data, sidebar2_data, sidebar3_data, footer_data } from './data';
 
 ReactDOM.render(
-    <div className="container">
+    <div>
         <div className="header">
             <Navbar {...navbar_data} />
         </div>
-        <div className="row">
-            <div className="col-sm-9 col-md-9 col-lg-9">
-                <Topbanner/>
-                <div className="center causelist-wrapper">
-                    <Causelist {...causelist_data} />
+        <div className="container">
+
+            <div className="row">
+                <div className="mainbar-wrapper">
+                    <Topbanner />
+                    <div className="center causelist-wrapper">
+                        <Causelist {...causelist_data} />
+                    </div>
+
+                </div>
+                <div className="sidebar-wrapper">
+                    <div>
+                        <Quicklink {...sidebar1_data} />
+
+                        <Quicklink2 {...sidebar2_data} />
+
+                        <Quicklink3 {...sidebar3_data} />
+                    </div>
                 </div>
 
             </div>
-            <div className="col-sm-3 col-md-3 col-lg-3 sidebar-wrapper">
-                <div>
-                    <Quicklink {...sidebar1_data} />
-
-                    <Quicklink2 {...sidebar2_data} />
-
-                    <Quicklink3 {...sidebar3_data} />
+            <div>
+                <div className="floatright">
+                    <button type="button" class="btn btn-info floatright"><i class="fas fa-print"></i>&nbsp;&nbsp;Print Case List</button>
                 </div>
-            </div>
+                <div>Total 9 cases found</div>
+                <div>Cause list generated</div>
 
-        </div>
-        <div>
-            <div className="floatright">
-                <button type="button" class="btn btn-info floatright"><i class="fas fa-print"></i>&nbsp;&nbsp;Print Case List</button>
             </div>
-            <div>Total 9 cases found</div>
-            <div>Cause list generated</div>
-
-        </div>
-        <div className="footer-wrapper">
-            <Footer {...footer_data} />
+            <div className="footer-wrapper">
+                <Footer {...footer_data} />
+            </div>
         </div>
     </div>,
 
